8a3ddf2d3e3eec7e9e3bd6c8765372f724782f43
notwindstone <notwindstone@gmail.com>
2026-06-26 18:39:18 +0300

    chore(deps): add biome linter

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,21 @@
-import DefaultTheme from 'vitepress/theme'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import { watch } from 'vue'
-import { inBrowser, type EnhanceAppContext } from 'vitepress/dist/client/index.js'
+import NProgress from "nprogress";
+import DefaultTheme from "vitepress/theme";
+import "nprogress/nprogress.css";
+import {
+  type EnhanceAppContext,
+  inBrowser,
+} from "vitepress/dist/client/index.js";
+import { watch } from "vue";
 import "./custom.css";
-import Status from "./components/table/Status.vue";
-import Ref from './components/table/Ref.vue'
+import Ref from "./components/table/Ref.vue";
 import Secondary from "./components/table/Secondary.vue";
+import Status from "./components/table/Status.vue";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app, router }: EnhanceAppContext) {
     app.component("Status", Status);
-    app.component('Ref', Ref);
+    app.component("Ref", Ref);
     app.component("Secondary", Secondary);
 
     // Otherwise, 'document' won't be defined
@@ -25,20 +28,19 @@ export default {
     NProgress.configure({ showSpinner: false });
 
     router.onBeforeRouteChange = (to: string) => {
-      const cleaned: string | undefined = to
-        ?.split("?")?.[0]
-        ?.split("#")?.[0];
+      const noSearchParams: string | undefined = to.split("?")[0];
+      const cleaned: string | undefined = noSearchParams?.split("#")?.[0];
 
       if (currentRoute !== cleaned) {
         NProgress.start();
       }
 
       currentRoute = cleaned;
-    }
+    };
 
     watch(
       () => router.route.path,
       () => NProgress.done(),
     );
-  }
-}
+  },
+};
